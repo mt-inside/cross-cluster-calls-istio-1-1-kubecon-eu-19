89df3062b84fedad4319a0562b7275c5dfd35b3f
Matt Turner <matturner@gmail.com>
2019-05-21 14:32:56 +0200

    Bump versions. Tested and works.

--- 00-versions.sh
+++ 00-versions.sh
@@ -1,8 +1,8 @@
-export K8S_VERSION=1.12
+export K8S_VERSION=1.13
 # Z set by 01z-shellz.sh
 export C=mlti-mesh
 
-export HELM_VERSION=2.13.1
+export HELM_VERSION=2.14.0
 export HELM=./darwin-amd64/helm
 
-export ISTIO_VERSION=1.1.5
+export ISTIO_VERSION=1.1.7
