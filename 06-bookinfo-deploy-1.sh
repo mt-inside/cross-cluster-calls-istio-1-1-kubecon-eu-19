77adbb39b8e89ccf0f7c7cfa167f45ae3895b23b
Matt Turner <matturner@gmail.com>
2019-07-04 22:59:20 -0400

    Remove unused bookinfo scripts

--- 06-bookinfo-deploy-1.sh
+++
@@ -1,6 +0,0 @@
-source 00-common.sh
-
-set -x
-kubectl apply -f istio/samples/bookinfo/platform/kube/bookinfo-a.yaml
-kubectl apply -f istio/samples/bookinfo/networking/bookinfo-gateway.yaml
-kubectl get pods --watch
--- 06-bookinfo-deploy-2.sh
+++
@@ -1,5 +0,0 @@
-source 00-common.sh
-
-set -x
-kubectl apply -f istio/samples/bookinfo/platform/kube/bookinfo-b.yaml
-kubectl get pods --watch
